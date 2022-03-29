c37a8219937589e6446a0d783b2e83261da92e96
Cameron Plauche <cplauche@wvup.edu>
2022-03-29 12:14:33 -0400

    Added DbSets

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ReqSystem.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,5 +13,21 @@ namespace ReqSystem.Data
             : base(options)
         {
         }
+
+        public DbSet<AcademicProgram> AcademicPrograms { get; set; }
+        public DbSet<Department> Addresses { get; set; }
+        public DbSet<Budget> Budgets { get; set; }
+        public DbSet<College> Colleges { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Division> Divisions { get; set; }
+        public DbSet<Fee> Fees { get; set; }
+        public DbSet<FileAttachment> FileAttachments { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Requisition> Requisitions { get; set; }
+        public DbSet<ReqUser> ReqUsers { get; set; }
+        public DbSet<Semester> Semesters { get; set; }
+        public DbSet<StateContract> StateContracts { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
     }
 }
