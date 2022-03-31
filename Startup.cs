942c61e25b479b87d686a78d3ae905a73bb4e1c0
Cameron Plauche <cplauche@wvup.edu>
2022-03-31 11:59:31 -0400

    Restored ReqUserRepoBase

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using DAL.IRepos;
+using DAL.Repos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -44,7 +45,7 @@ namespace ReqSystem
             services.AddScoped<IRepo<FileAttachment>, FileAttachmentRepo>();
             services.AddScoped<IRepo<Item>, ItemRepo>();
             services.AddScoped<IRepo<Requisition>, RequisitionRepo>();
-            services.AddScoped<IReqUserRepo<ReqUser>, ReqUserRepo>();
+            services.AddScoped<IReqUserRepo<ReqUser>, ReqUserRepoBase>();
             services.AddScoped<IRepo<StateContract>, StateContractRepo>();
             services.AddScoped<IRepo<Vendor>, VendorRepo>();
 
